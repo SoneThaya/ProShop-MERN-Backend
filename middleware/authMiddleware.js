6d2af5fe9e6e9e0e8e99b937b2d360cfa216c8e0
Sone <sthaya@gmail.com>
2020-11-07 14:02:18 -0800

    set up protected route

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -14,10 +14,13 @@ const protect = asyncHandler(async (req, res, next) => {
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      console.log(decoded);
+      req.user = await User.findById(decoded.id).select("-password");
+
       next();
     } catch (error) {
-      console.log(error);
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
     }
   }
 
