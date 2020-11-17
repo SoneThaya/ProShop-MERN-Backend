ca80d286faad9598d73d6a7e44fad5cc8189b011
Sone <sthaya@gmail.com>
2020-11-16 19:53:19 -0800

    installed cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import colors from "colors";
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import cors from "cors";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -20,6 +21,8 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+app.use(cors());
 
 app.use(express.json());
 
